0e70dfc293c8712e817344c3319211e5b05be2ec
naman <namansingh48@gmail.com>
2024-04-03 13:45:40 +0530

    added test case for subtraction method

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,4 +6,7 @@ public class CalculatorTest extends TestCase {
    public void testadd() {
    assertEquals(2,cal.add(1,1));
   }
+  public void testsub() {
+    assertEquals(2,cal.sub(1,1));
+   }
 }
